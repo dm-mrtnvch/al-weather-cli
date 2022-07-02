59b6d176b9b47596a7aa24fdbcd09d7f1ab7a041
dm-mrtnvch <dmvshn@gmail.com>
2022-07-02 14:25:16 +0400

    os & path

---
+++ services/log.service.js
@@ -0,0 +1,25 @@
+import chalk from 'chalk'
+import dedent from 'dedent'
+
+
+const {bgGreen, bgRed, bgCyan} = chalk
+
+const printSuccess = (message) => {
+  console.log(bgGreen('SUCCESS: '), message)
+}
+
+const printError = (error) => {
+  console.log(bgRed('ERROR: '), error)
+}
+
+const printHelp = () => {
+  console.log(
+    dedent(`${bgCyan(' HELP ')}
+    without params - weather input
+    -s [CITY] to set a city
+    -h for help
+    -t [API_KEY] to save token`
+  ))
+}
+
+export {printSuccess, printError, printHelp}
--- weather.js
+++ weather.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env mode
+import os from 'os'
 import getArgs from './helpers/args.js';
+import {printHelp} from './services/log.service.js';
+import {saveKeyValue} from './services/storage.service.js';
 
 const initCLI = () => {
   const args = getArgs(process.argv)
-  console.log(args)
+  // console.log(args)
   if(args.h) {
-    // help
+    printHelp()
   }
   if(args.s){
     // save city
   }
   if(args.t){
-    // save token
+    saveKeyValue('token', args.t)
   }
   // show weather
 }
